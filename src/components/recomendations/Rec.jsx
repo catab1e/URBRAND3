132066260a9cdfe1479c4596a3c48dac8b58cecd
Sary Sary <tilektursunbaiuulu@gmail.com>
2023-03-28 03:11:46 +0600

    added adaptive rec-box

--- src/components/recomendations/Rec.jsx
+++ src/components/recomendations/Rec.jsx
@@ -13,6 +13,43 @@ const Rec = () => {
 
       <div className="rec__item item-1">
         <img src={orange} alt="" />
+        <div className="black-filter">
+          <p className="rec__descr">ПОПУЛЯРНОЕ</p>
+        </div>
+      </div>
+
+      <div className="rec__right">
+        <div className="col">
+          <div className="rec__item item-2">
+            <img className="rec__img" src={ornaments} alt="" />
+            <div className="black-filter">
+              <p className="rec__descr">KG</p>
+            </div> 
+          </div>
+
+          <div className="rec__item item-3">
+            <img className="rec__img" src={AI} alt="" />
+            <div className="black-filter">
+              <p className="rec__descr">ОТ НЕЙРОСЕТИ</p>
+            </div>
+          </div>
+        </div>
+        {/* column */}
+        <div className="col">
+          <div className="rec__item item-3">
+            <img className="rec__img" src={oneme} alt="" />
+            <div className="black-filter">
+              <p className="rec__descr">АНИМЕ</p>
+            </div>
+          </div>
+
+          <div className="rec__item item-2">
+            <img className="rec__img" src={dialog} alt="" />
+            <div className="black-filter">
+              <p className="rec__descr">ЦИТАТЫ</p>
+            </div>
+          </div>
+        </div>
       </div>
 
     </div>
