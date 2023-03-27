a16bac3aef9598b99d5ccac919a25ee75b2752f4
Sary Sary <tilektursunbaiuulu@gmail.com>
2023-03-27 23:23:55 +0600

    re-make rec-box

--- src/components/recomendations/Rec.jsx
+++ src/components/recomendations/Rec.jsx
@@ -9,48 +9,12 @@ import ornaments from "../assets/img/ornaments.png";
 
 const Rec = () => {
   return (
-    <div className="rec">
-      <div className="rec-1 wrapper rel item">
-        <div className="abs">
-          <img className="img" src={orange} alt="" />
-        </div>
+    <div className="rec-box">
 
-        <div className="black-filter">ПОПУЛЯРНОЕ</div>
+      <div className="rec__item item-1">
+        <img src={orange} alt="" />
       </div>
 
-      <div className="rec-2-container">
-        <div className="rec-2 wrapper">
-          <div className="rec-2-1 wrapper rel item">
-            <div className="abs">
-              <img className="img" src={ornaments} alt="" />
-            </div>
-            <div className="black-filter">KG</div>
-          </div>
-          <div className="rec-2-2 wrapper rel item">
-            <div className="abs">
-              <img className="img" src={AI} alt="" />
-            </div>
-            <div className="black-filter">ОТ НЕЙРОСЕТИ</div>
-          </div>
-        </div>
-
-        <div className="rec-3 wrapper">
-          <div className="rec-3-1 wrapper rel item">
-            <div className="abs">
-              <img className="img" src={oneme} alt="" />
-            </div>
-            <div className="black-filter">АНИМЕ</div>
-          </div>
-          <div className="rec-3-2 wrapper rel item">
-            <div className="abs">
-              <img className="img" src={dialog} alt="" />
-            </div>
-            <div className="black-filter">ЦИТАТЫ</div>
-          </div>
-        </div>
-      </div>
-
-      
     </div>
   );
 };
